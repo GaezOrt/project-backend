e1dee42b93cec67d1977f6cbfc540cb3ca3df445
Gaezort <gaezort@gmail.com>
2022-04-11 10:56:35 -0300

    agrego

--- express.js
+++ express.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const server=express();
-
+const mysql = require('mysql');
 
 server.use(express.json())
 
@@ -10,12 +10,61 @@ server.get('/xx',function(req,res){
 })
 
 server.post('/login',function(req,res){
+    let con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'prueba'
+    });
+    con.connect();
+    var sql = "INSERT INTO clients (name, password) VALUES ?";
+    var values = [
+      [req.body.username, req.body.password]
+    ];
+    con.query(sql, [values], function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+      });
 
-    
-    console.log(req.body);
-    res.send({id:1,name:'Nombre'});
-    res.end();
+   // res.send({id:1,name:'Nombre'});
+    //res.end();
 })
+
+server.post('/register',function(req,res){
+    let con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'prueba'
+    });
+    con.connect();
+
+    var username = req.body.username;
+    var sql = 'SELECT * FROM clients WHERE name = '+ mysql.escape(username);
+    con.query(sql, [username], function (err, result) {
+        if (err) throw err;
+        console.log(result.affectedRows);
+        if(result.affectedRows!=null){
+            console.log('Existe registor ya');
+        }else{
+            console.log('No existe registro')
+            var sql = "INSERT INTO clients (name, password) VALUES ?";
+            var values = [
+              [req.body.username, req.body.password]
+            ];
+            con.query(sql, [values], function (err, result) {
+                if (err) throw err;
+                console.log("Number of records inserted: " + result.affectedRows);
+              });
+        }
+      });
+    
+    
+    
+   // res.send({id:1,name:'Nombre'});
+    //res.end();
+})
+
 
 /*server.post('/login',(req,res)=>{
     console.log(req);
@@ -26,7 +75,7 @@ server.post('/login',function(req,res){
 })
 
 */
-
+ 
 
 server.listen(3000,function(){
     console.log('Port 3000')
